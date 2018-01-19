52a8ea89c3bff70b61977a676f6e137b78616511
Georgi Lyubenov <godzbanebane@gmail.com>
2018-01-19 23:50:14 +0200

    Insert for Trie
    Added two inserts for the Trie, the second one being cleaner
    than the first one, but unsure whether it's better or the
    abstraction is unneeded. Also they typecheck but are untested.

--- trie.hs
+++ trie.hs
@@ -1,7 +1,9 @@
-import qualified Data.Map.Lazy as Lazy
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
+import Data.Maybe
 
 data Trie a = Empty 
-            | Node (Maybe a) (Lazy.Map Char (Trie a)) 
+            | Node (Maybe a) (Map Char (Trie a)) 
                 deriving (Show, Eq)
 
 instance Functor Trie where
@@ -11,3 +13,32 @@ instance Functor Trie where
 instance Foldable Trie where
     foldMap g Empty = mempty
     foldMap g (Node val map) = foldMap g val `mappend` foldMap (foldMap g) map
+
+-- abstract above the value of a Node, so I can
+-- write two cases as one in insert
+nodeVal :: Trie a -> Maybe a
+nodeVal Empty        = Nothing
+nodeVal (Node val _) = val
+
+-- same as above
+nodeMap :: Trie a -> Map Char (Trie a)
+nodeMap Empty        = Map.empty
+nodeMap (Node _ map) = map
+
+-- manually looking at cases
+insert :: String -> a -> Trie a -> Trie a
+insert [] val Empty                  = Node (Just val) Map.empty
+insert [] val (Node currval map)     = Node (Just val) map
+
+insert (x:xs) val Empty              = Node Nothing (Map.insert x (insert xs val Empty) Map.empty)
+insert (x:xs) val (Node currval map) = Node currval inserted
+                                        where next     = fromMaybe Empty $ Map.lookup x map
+                                              inserted = Map.insert x (insert xs val next) map
+
+-- recursing only on the string
+insert' :: String -> a -> Trie a -> Trie a
+insert' [] val tree     = Node (Just val) $ nodeMap tree
+insert' (x:xs) val tree = Node (nodeVal tree) inserted
+                           where map      = nodeMap tree
+                                 next     = fromMaybe Empty $ Map.lookup x map
+                                 inserted = Map.insert x (insert' xs val next) map

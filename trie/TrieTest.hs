3a0946afa280be3c910a9c62a388bcb8512e165e
Georgi Lyubenov <godzbanebane@gmail.com>
2018-01-20 21:52:22 +0200

    Tests for contains and insert
    Added tests for contains and multiple unique key insertion,
    however they depend on each other which sucks.

--- trie/TrieTest.hs
+++ trie/TrieTest.hs
@@ -4,7 +4,7 @@ import Trie (Trie(..))
 import qualified Trie as T
 import Test.QuickCheck
 import Test.QuickCheck.All
-import Data.Map.Lazy
+import qualified Data.Map.Lazy as M
 import Data.Maybe
 
 instance (Arbitrary a) => Arbitrary (Trie a) where
@@ -20,14 +20,46 @@ arbitraryTrie m = do
     n <- choose (0, m `div` 2)
     keys <- vectorOf n arbitrary
     vals <- vectorOf n $ arbitraryTrie $ m `div` 4
-    return (Node ma (fromList $ zip keys vals))
+    return (Node ma (M.fromList $ zip keys vals))
 
 
-prop_insertWorks :: (Eq a) => String -> a -> Trie a -> Bool
-prop_insertWorks key val trie = (T.lookup key $ T.insert key val trie) == (Just val)
+-- testing if contains actually works
+-- have circular dependency here, because contains
+-- depends on insert working and vice versa
+-- for the test to be valid
+prop_contains :: (Eq a) => String -> a -> Trie a -> Bool
+prop_contains key val trie = T.contains key val $ T.insert key val trie
 
+-- testing if lookup in an empty Trie fails
 prop_emptyTrieIsEmpty :: String -> Bool
 prop_emptyTrieIsEmpty key = isNothing $ T.lookup key Empty
+
+-- checking multiple insertion of unique keys for insert
+prop_insertion :: (Eq a) => [String] -> [a] -> Trie a -> Property
+prop_insertion keys vals trie = (not $ duplicate keys) ==> all (==True) contained
+
+    where zipped           = zip keys vals
+          insertZip        = flip $ uncurry T.insert
+          inserted         = foldl insertZip trie zipped
+          containsZip      = flip $ uncurry T.contains
+          contained        = map (containsZip inserted) zipped
+          duplicate []     = False
+          duplicate (x:xs) = elem x xs || duplicate xs
+
+-- checking multiple insertion of unique keys for insert'
+prop_insertion' :: (Eq a) => [String] -> [a] -> Trie a -> Property
+prop_insertion' keys vals trie = (not $ duplicate keys) ==> all (==True) contained
+
+    where zipped           = zip keys vals
+          insertZip        = flip $ uncurry T.insert'
+          inserted         = foldl insertZip trie zipped
+          containsZip      = flip $ uncurry T.contains
+          contained        = map (containsZip inserted) zipped
+          duplicate []     = False
+          duplicate (x:xs) = elem x xs || duplicate xs
+
+--insertZip :: Trie a -> (String, a) ->  Trie a
+--insertZip trie (key, val) = T.insert key val trie
 
 return []
 

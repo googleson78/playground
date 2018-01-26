8f818c75c5837e5c412b95ce286c509561bedffd
Georgi Lyubenov <godzbanebane@gmail.com>
2018-01-26 11:04:46 +0200

    some styling changes

--- determinant/Det.hs
+++ determinant/Det.hs
@@ -4,28 +4,32 @@ module Det
 
 countinv :: [Int] -> Int
 countinv [] = 0
-countinv (x:xs) = length [z | z <- xs, x > z] + countinv xs
+countinv (x:xs) = (length $ filter (<x) xs) + countinv xs
 
 perm :: [Int] -> [[Int]]
 perm [] = [[]]
-perm (x:xs) = concat $ map (insertAll x) $ perm xs
+perm (x:xs) = concat $ map (insertAtAllPlaces x) $ perm xs
 
-insertAll :: a -> [a] -> [[a]]
-insertAll x [] = [[x]]
-insertAll x ys = zipWith (insertAt x) [0..m] yss
-                    where m = length ys + 1
-                          yss = replicate m ys
+insertAtAllPlaces :: a -> [a] -> [[a]]
+insertAtAllPlaces x [] = [[x]]
+insertAtAllPlaces x ys = zipWith (insertAt x) [0..m] yss
+                           where m = length ys + 1
+                                 yss = replicate m ys
 
 insertAt :: a -> Int -> [a] -> [a]
 insertAt x 0 ys     = x:ys
-insertAt _ _ []     = undefined
 insertAt x n (y:ys) = y:(insertAt x (n - 1) ys)
 
 takePermInd :: [Int] -> [[Int]] -> [Int] 
 takePermInd = zipWith (flip (!!))
 
 det :: [[Int]] -> Int
-det [[x]] = x
-det xss = sum [((-1) ^ (countinv perm)) * (product $ takePermInd perm xss) | perm <- perms]
+det xss = sum permProducts 
                 where n = length xss
-                      perms = perm [0..(n - 1)]
+                      numberPerms = perm [0..(n - 1)]
+
+                      singleProdFormula = (\ perm ->
+                                          ((-1) ^ (countinv perm)) *
+                                          (product $ takePermInd perm xss))
+
+                      permProducts = map singleProdFormula numberPerms

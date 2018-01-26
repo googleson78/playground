e850d596d3f2c9810b394237990772a3f42df6bd
Georgi Lyubenov <godzbanebane@gmail.com>
2018-01-26 12:38:57 +0200

    made some functions polymorphic, added comments

--- determinant/Det.hs
+++ determinant/Det.hs
@@ -2,11 +2,21 @@ module Det
     (det) 
     where
 
+import Data.Monoid
+
+-- counts the inversions of a list
+-- in the most "wooden" way possible
+-- (using the definition)
 countinv :: [Int] -> Int
 countinv [] = 0
 countinv (x:xs) = (length $ filter (<x) xs) + countinv xs
 
-perm :: [Int] -> [[Int]]
+-- all permutations of a list
+-- again, very basic way 
+-- a permutation of (x:xs) is inserting
+-- x at every possible "spot", and that is
+-- for each permutation of xs
+perm :: [a] -> [[a]]
 perm [] = [[]]
 perm (x:xs) = concat $ map (insertAtAllPlaces x) $ perm xs
 
@@ -16,11 +26,17 @@ insertAtAllPlaces x ys = zipWith (insertAt x) [0..m] yss
                            where m = length ys + 1
                                  yss = replicate m ys
 
+-- as required by perm
 insertAt :: a -> Int -> [a] -> [a]
 insertAt x 0 ys     = x:ys
 insertAt x n (y:ys) = y:(insertAt x (n - 1) ys)
 
-takePermInd :: [Int] -> [[Int]] -> [Int] 
+-- purely mechanical thing required for dets 
+-- when making a single product of the sum of the entire det
+-- 
+-- take a list of indices and uses them to index
+-- each of the lists in a list
+takePermInd :: [Int] -> [[a]] -> [a] 
 takePermInd = zipWith (flip (!!))
 
 det :: [[Int]] -> Int

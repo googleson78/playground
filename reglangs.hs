eee16bbacde3660be9a3f5fff53ee2a3c4fdfac0
Georgi Lyubenov <godzbanebane@gmail.com>
2018-03-16 16:40:21 +0200

    Fixed language concatenation
    Finally fixed concatenation by relying on indexing both langs
    and then outputting their words via diagonalization (all
    words whose indices sum to less than n, for all n, allowing
    finite time for any word from a*b* for example).
    Brute force recognition also works now as a consequence.

--- reglangs.hs
+++ reglangs.hs
@@ -1,4 +1,6 @@
 import Data.List
+import Control.Applicative
+import Data.Maybe
 
 type Var = Int
 type Const = Char
@@ -41,8 +43,29 @@ instance Monoid RegEx where
 eps :: String
 eps = []
 
+-- 
+(!!!) :: [a] -> Int -> Maybe a
+(!!!) [] _     = Nothing
+(!!!) (x:_) 0  = Just x
+(!!!) (_:xs) n = xs !!! (pred n)
+
+diags :: [(Int, Int)]
+diags = concatMap genPairs [0..]
+    where genPairs n = zip [n, n - 1..0] [0..n]
+
+zip2Tuple :: (a -> b, c -> d) -> (a, c) -> (b, d)
+zip2Tuple (f, g) (x, y) = (f x, g y)
+
+
+-- forgive me lord, for I have sinned (?)
+--
+-- TODO: ADD EXPLANATION AND PROOF SKETCH
+-- basically relies on diagonalization to create an iso between (N) and (N x N)
 concat' :: Lang -> Lang -> Lang
-concat' l1 l2 = [a ++ b | a <- l1, b <- l2]
+concat' xs ys = ap'ed >>= maybeToList
+    where indexed     = map (zip2Tuple ((xs!!!), (ys!!!))) diags
+          exhausted   = takeWhile (/= (Nothing, Nothing)) indexed
+          ap'ed       = map (uncurry (liftA2 (++))) exhausted
 
 regLang :: RegEx -> Lang
 regLang EmptySet       =    []
@@ -57,17 +80,24 @@ regLang (Concat r1 r2) =    lang1 `concat'` lang2
                                 where lang1 = regLang r1
                                       lang2 = regLang r2
 
--- THIS DOES NOT WORK WELL
 regLang (Kleene r)     =    concat $ iterate (lang `concat'`) [eps]
                                 where lang = regLang r
 
--- needs fixing for stuff like b*c*
--- recognizes :: RegEx -> String -> Bool
--- recognizes rx w = elem w $ filter (((length w) ==) . length) (regLang rx)
+recognizes :: String -> RegEx -> Bool
+recognizes w = elem w . lengthLessThanW . regLang
+    where wl              = length w
+          lengthLessThanW = takeWhile ((<=wl) . length)
+
 
 a = Sing 'a'
 b = Sing 'b'
 c = Sing 'c'
+k = Sing 'k'
+ab = Union a b
+bc = Union b c
+abbc = Concat ab bc
+abbca = Union abbc a
+abbcak = Concat abbca k
 aORb = Union a b
 aORc = Union a c
 cKleene = Kleene c
@@ -77,3 +107,5 @@ reg1 = Concat cKleene aORb
 reg2 = Concat aORb aORc
 reg3 = Concat reg1 reg2
 reg4 = Union reg3 reg1
+reg5 = (Kleene bKconcatcK)
+reg6 = (Concat reg5 a)

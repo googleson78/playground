3bf43fe214e2dfc2aa2adf19782fa70fb6550367
Georgi Lyubenov <georgi.lyubenov@chaosgroup.com>
2018-09-21 15:59:12 +0300

    Add records and generic FromJSON testing

--- yaml.hs
+++ yaml.hs
@@ -6,6 +6,8 @@ import GHC.Generics
 import Data.Text
 import Data.Yaml
 import Data.Yaml.TH
+import Data.Aeson
+import Data.Char (toLower)
 
 data Config = Paths [Text]
             | Allowed [Text]
@@ -23,7 +25,29 @@ instance FromJSON A where
         t <- o .:? "non" :: Parser (Maybe Value)
         pure (t `seq` A)
 
+data RecordsTest =
+    RecordsTest
+        { rtA :: Text
+        , rtB :: Maybe Int
+        }
+    deriving (Show, Eq, Generic)
+
+rtOpts :: Options
+rtOpts = defaultOptions { fieldLabelModifier = process
+                        , omitNothingFields  = True
+                        }
+    where process (x:y:z:xs) = Data.Char.toLower z : xs
+
+instance ToJSON RecordsTest where
+    toJSON = genericToJSON rtOpts
+
+instance FromJSON RecordsTest where
+    parseJSON = genericParseJSON rtOpts
+
 a = Paths ["/lolJK/kek", "najs, aaaa"]
+
+asdf' :: Value
+asdf' = [yamlQQ|a: lol|]
 
 b :: Value
 b = [yamlQQ|

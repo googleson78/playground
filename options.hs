d0c5fa9eafff84fb655969aa394f7b42eb36c394
Georgi Lyubenov <godzbanebane@gmail.com>
2019-01-10 23:58:41 +0200

    Add some more playing around, with reification now

--- options.hs
+++ options.hs
@@ -6,10 +6,14 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
 
 import Data.Reflection
 
 import Data.Proxy
+
+import GHC.TypeLits
 
 data Options
     = FirstParent
@@ -24,6 +28,19 @@ instance ToArg FirstParent where
 instance ToArg CherryPick where
     toArg _ = "--cherry-pick"
 
+fromArg :: String -> (forall a. ToArg a => Proxy a -> r) -> r
+fromArg "--cherry-pick" cont = cont (Proxy :: Proxy CherryPick)
+fromArg "--first-parent" cont = cont (Proxy :: Proxy FirstParent)
+
+fromArgs :: [String] -> (forall a. ToArgs a => Proxy a -> r) -> r
+fromArgs []     cont = cont (Proxy :: Proxy '[])
+fromArgs ("--cherry-pick":xs) cont = fromArgs xs $ \(Proxy :: Proxy r) -> cont (Proxy :: Proxy (CherryPick ': r))
+fromArgs ("--first-parent":xs) cont = fromArgs xs $ \(Proxy :: Proxy r) -> cont (Proxy :: Proxy (FirstParent ': r))
+
+data family Files (a :: Options)
+data instance Files FirstParent = None
+data instance Files CherryPick = Some String
+
 class ToArgs (a :: [Options]) where
     toArgs :: Proxy a -> [String]
 
@@ -37,8 +54,15 @@ data Calls
     = Normal
     | WithFiles
 
-newtype OptionsS = O [String]
+newtype OptionsS = O String
     deriving (Show)
 
-instance Reifies '[FirstParent] OptionsS where
-    reflect p = O $ toArgs (Proxy :: Proxy '[FirstParent])
+instance Reifies Options OptionsS where
+    reflect p = undefined
+
+type family Match (a :: Symbol) :: Options where
+    Match "--first-parent" = FirstParent
+    Match "--cherry-pick" = CherryPick
+
+--f :: forall s. (Reifies s [Char], ToArg s) => Proxy Symbol -> IO ()
+--f _ = putStrLn $ toArg (Proxy :: Proxy (Match s))

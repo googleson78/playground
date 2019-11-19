4de079726eadac55b0644eb03f45924b1c438be9
Georgi Lyubenov <godzbanebane@gmail.com>
2019-11-19 16:38:32 +0200

    Better name for SumArgs

--- varargs.hs
+++ varargs.hs
@@ -1,12 +1,12 @@
-class SumArgs a where
+class SumResult a where
     sappend :: [Integer] -> a
 
-instance (Integral a, SumArgs r) => SumArgs (a -> r) where
+instance (Integral a, SumResult r) => SumResult (a -> r) where
     --sappend acc = \x -> sappend (toInteger x : acc)
     sappend acc = sappend . (:acc) . toInteger
 
-instance SumArgs Integer where
+instance SumResult Integer where
     sappend = sum
 
-plus :: (SumArgs a) => a
+plus :: (SumResult a) => a
 plus = sappend []
